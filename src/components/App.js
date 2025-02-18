81620a999a6b7070ff44ddb4644d3825175a7f76
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-02-18 17:49:59 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Link, Route, Routes} from "reac-router-dom"
+
+const Header = () => {
+  return (
+    <header>
+        {/* Do not remove the main div */}
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+    </header>
+  )
+};
+
+const Home = () => {
+  return (
+    <h1>Welcome to my website!</h1>
+  )
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is a sample React Router program.</h1>
+    </div>
+  )
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
