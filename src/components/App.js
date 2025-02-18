49957cb2c862b342680f4520c8c7f6450e95cb36
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-02-18 18:27:35 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
-
 import React from "react";
-import './../styles/App.css';
-import { BrowserRouter, Link, Route, Routes} from "reac-router-dom"
+import "./../styles/App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-        {/* Do not remove the main div */}
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
+      {/* Do not remove the main div */}
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </header>
-  )
+  );
 };
 
 const Home = () => {
-  return (
-    <h1>Welcome to my website!</h1>
-  )
+  return <h1>Welcome to my website!</h1>;
 };
 
 const About = () => {
@@ -26,19 +27,19 @@ const About = () => {
     <div>
       <h1>This is a sample React Router program.</h1>
     </div>
-  )
+  );
 };
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
